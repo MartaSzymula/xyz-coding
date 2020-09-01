5156be849c4c971232d474cd7147d436f5f873a6
martaszy <martaszymula2@gmail.com>
2020-09-01 22:56:15 +0300

    Social app project - relationships practice. Initial setup & user and posts class done.

---
+++ web_dev/relationships/social_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+    content = models.CharField(max_length=255)
+    user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
