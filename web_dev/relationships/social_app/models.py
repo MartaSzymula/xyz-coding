b35e6b069b62a7a568634023a895044579c0f526
martaszy <martaszymula2@gmail.com>
2020-09-03 19:36:04 +0300

    wip

--- web_dev/relationships/social_app/models.py
+++ web_dev/relationships/social_app/models.py
@@ -10,5 +10,6 @@ class User(models.Model):
 class Post(models.Model):
     content = models.CharField(max_length=255)
     user = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name="posts_liked")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
